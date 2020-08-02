8e39b14ae4fd41f08e14c42772ae00bc9891e813
Thamal Dilanka <thamaldilanke@gmail.com>
2020-08-02 14:58:51 +0530

    Project structure initialized and connected to the database

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+
+// Importing routes from the routes folder
+
+
+// Creating a Express application
+const app = express();
+
+// CORS support
+app.use(cors());
+
+// Setting body parser to get access of request.body
+app.use(bodyParser.json());
+
+// Logging all the requests to the console in development environment
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
+// Setting the routes to the app as middlewares
+
+
+// Setting static webpage
+app.use(express.static('./client'));
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+});
+
+module.exports = app;
