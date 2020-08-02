ee98653c31e20a2e31877b481a5eec7a1acbcd71
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2020-08-02 19:40:21 +0530

    CRUD for Group IDs

---
+++ models/GroupID.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const groupidSchema = new mongoose.Schema({
+  yearsemestername: {
+    type: String,
+    trim: true,
+    required: [true, "YearSemester is Mandatory!"],
+  },
+  specializationname: {
+    type: String,
+    trim: true,
+    required: [true, "Specialization Name is Mandatory!"],
+  },
+  groupnumber: {
+    type: Number,
+    min:1,
+    trim: true,
+    required: [true, "GroupNumber is Mandatory!"],
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const GroupID = mongoose.model("groupid", groupidSchema);
+module.exports = GroupID;
