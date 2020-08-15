0d7fa15e076f7d4531a6c6cd81618458895307ce
Dinuka Shameera <dinukashameera0657@gmail.com>
2020-08-15 15:30:59 +0530

    changes working day model and route

--- controllers/workingDaysController.js
+++ controllers/workingDaysController.js
@@ -1,22 +1,35 @@
 const WorkingDays = require("../models/workingDays");
 
 exports.createWorkingDays = async (req, res) => {
-  console.log("hello world");
-  let { workingDayType, noOfWorkingDays, workingDay, timeSlot } = req.body;
+  console.log(req);
+  let {
+    dayType,
+    noOfWorkingDays,
+    dayOfWork,
+    timeSlot,
+    fromTime,
+    toTime,
+    workingHours,
+    workingMins,
+  } = req.body;
   try {
     const newWorkingDays = new WorkingDays({
-      workingDayType,
+      dayType,
       noOfWorkingDays,
-      workingDay,
+      dayOfWork,
       timeSlot,
+      fromTime,
+      toTime,
+      workingHours,
+      workingMins,
     });
 
     const result = await newWorkingDays.save();
-    console.log(result);
+    // console.log(result);
     res.send(result);
   } catch (error) {
     res.send(error);
-    console.error(error);
+    console.error("These Are the errors in console", error);
   }
 };
 
@@ -30,65 +43,73 @@ exports.getAllWorkingDays = async (req, res) => {
   }
 };
 
-exports.getWorkingDay = async(req,res) => {
+exports.getWorkingDay = async (req, res) => {
   try {
-    const day = await WorkingDays.findById({_id : req.params.id});
-    if(!day){
-      res.send({msg : 'Working Day is Not available'})
-      console.log(day)
+    const day = await WorkingDays.findById({ _id: req.params.id });
+    if (!day) {
+      res.send({ msg: "Working Day is Not available" });
+      console.log(day);
     }
   } catch (error) {
-    res.send(error)
-    console.log(error)
+    res.send(error);
+    console.log(error);
   }
-}
+};
 
-
-exports.deleteWorkingDay = async(req,res) => {
+exports.deleteWorkingDay = async (req, res) => {
   try {
-    const isAvailable = await WorkingDays.findById({_id : req.params.id});
-    if(!isAvailable){
-      res.send({msg : 'Working Day is Not available'})
-      console.log(isAvailable)
+    const isAvailable = await WorkingDays.findById({ _id: req.params.id });
+    if (!isAvailable) {
+      res.send({ msg: "Working Day is Not available" });
+      console.log(isAvailable);
     }
 
-    const day = await isAvailable.remove()
-    res.send(day)
-    console.log(day)
+    const day = await isAvailable.remove();
+    res.send(day);
+    console.log(day);
   } catch (error) {
-    res.send(error)
-    console.log(error)
+    res.send(error);
+    console.log(error);
   }
-}
+};
 
-exports.updateWorkingDay = async(req,res) => {
-  let { workingDayType, noOfWorkingDays, workingDay, timeSlot } = req.body;
+exports.updateWorkingDay = async (req, res) => {
+  console.log(req)
+  console.log(req.params.id )
+  let {
+    dayType,
+    noOfWorkingDays,
+    dayOfWork,
+    timeSlot,
+    fromTime,
+    toTime,
+    workingHours,
+    workingMins,
+  } = req.body
 
   try {
-    const isAvailable = await WorkingDays.findById({_id : req.params.id});
-    if(!isAvailable){
-      res.send({msg : 'Working Day is Not available'})
-      console.log(isAvailable)
+    const isAvailable = await WorkingDays.findById({ _id: req.params.id });
+    if (!isAvailable) {
+      res.send({ msg: "Working Day is Not available" });
+      console.log(isAvailable);
     }
 
     isAvailable.set({
-      workingDayType, 
-      noOfWorkingDays, 
-      workingDay, 
-      timeSlot
+      dayType,
+      noOfWorkingDays,
+      dayOfWork,
+      timeSlot,
+      fromTime,
+      toTime,
+      workingHours,
+      workingMins,
     });
 
     const result = await isAvailable.save();
-    res.send(result)
-    console.log(result)
-
+    res.send(result);
+    console.log(result);
   } catch (error) {
-    res.send(error)
-    console.log(error)
+    res.send(error);
+    console.log(error);
   }
-}
-
-
-
-
-
+};
