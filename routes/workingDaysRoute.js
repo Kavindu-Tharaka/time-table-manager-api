79fdc2e681dbf987eddedb78e1c984e515a8e0f6
Dinuka Shameera <dinukashameera0657@gmail.com>
2020-08-16 21:28:39 +0530

    added get count functionality

--- routes/workingDaysRoute.js
+++ routes/workingDaysRoute.js
@@ -4,9 +4,11 @@ const workingDaysController = require('../controllers/workingDaysController')
 
 router.route('/').post(workingDaysController.createWorkingDays);
 router.route('/').get(workingDaysController.getAllWorkingDays);
-router.route('/:id').get(workingDaysController.getWorkingDay)
-router.route('/:id').delete(workingDaysController.deleteWorkingDay)
-router.route('/:id').patch(workingDaysController.updateWorkingDay)
+router.route('/:id').get(workingDaysController.getWorkingDay);
+router.route('/:id').delete(workingDaysController.deleteWorkingDay);
+router.route('/:id').patch(workingDaysController.updateWorkingDay);
+router.route('/type/count/:type').get(workingDaysController.getDayTypeCount);
+
 
 module.exports = router
 
