e34515ebee5cf007789ff1639c5bf2790e8adb35
Kavindu-Tharaka <kavindu.ktm@gmail.com>
2020-09-23 22:37:51 +0530

    finilized cruds version 2 for sprint 2

--- models/ConstraintsConsecutiveSession.js
+++ models/ConstraintsConsecutiveSession.js
@@ -10,18 +10,15 @@ const ConstraintsConsecutiveSessionSchema = new Schema({
         type: Number,
         min: 1,
         max: 4,
-        required: [true, 'Year is Mandatory!']
     },
     semester: {
         type: Number,
         min: 1,
         max: 2,
-        required: [true, 'Semester is Mandatory!']
     },
     subject: {
         type: Schema.Types.ObjectId,
         ref: Subject,
-        required: [true, 'Subject is Mandatory!']
     },
     consecutivesessions: [{
         type: Schema.Types.ObjectId,
